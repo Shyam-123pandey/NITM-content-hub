30dc9f284dfee31fcf903eab40f09746cc145dd3
shyam pandey <shyampandey2625@gmail.com>
2025-05-14 19:30:57 +0530

    after all cores

--- Server/server.js
+++ Server/server.js
@@ -19,10 +19,7 @@ if (!fs.existsSync(uploadDir)) {
 }
 
 // Middleware
-app.use(cors({
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
-    credentials: true
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
