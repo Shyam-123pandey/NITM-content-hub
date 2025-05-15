c4f1614e2af42e43a6220ea1e8b85b462021dfe3
shyam pandey <shyampandey2625@gmail.com>
2025-05-15 10:10:17 +0530

    set after cors

--- Server/index.js
+++ Server/index.js
@@ -38,7 +38,7 @@ app.use(session({
 
 // CORS configuration
 app.use(cors({
-    origin: true, // Allow all origins
+    origin: "https://nitm-content-platform.netlify.app", // Allow all origins
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
