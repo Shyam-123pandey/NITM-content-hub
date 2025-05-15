668888af0139b14e1557628ff99e59000990503d
shyam pandey <shyampandey2625@gmail.com>
2025-05-15 10:42:24 +0530

    After cors

--- Server/index.js
+++ Server/index.js
@@ -38,12 +38,7 @@ app.use(session({
 
 // CORS configuration
 app.use(cors({
-    origin: "https://nitm-content-platform.netlify.app", // Allow all origins
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    exposedHeaders: ['Content-Range', 'X-Content-Range'],
-    maxAge: 600 // Cache preflight request for 10 minutes
+  origin: 'https://nitm-content-platform.netlify.app' // your Netlify frontend
 }));
 
 app.use(express.json());
